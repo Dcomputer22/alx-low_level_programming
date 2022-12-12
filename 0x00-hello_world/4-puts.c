af4b58a5045823976f76a56fa900c88607e54844
Fatima <fatimaoyiza18@gmail.com>
2022-12-12 01:58:14 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,8 @@
  *Description: we are unable to use printf so we must use puts to print the string
  *Return: Code should return a 0
  */
-int main(void) {
-	puts("\"Programming is like building a multilingual puzzle");
+int main(void) 
+{
+	puts ("\"Programming is like building a multilingual puzzle");
 		return(0);
 }
