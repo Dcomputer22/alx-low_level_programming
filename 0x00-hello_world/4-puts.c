21e0fb9c07dd3566d6a9a01aba72ffaf94665e44
Fatima <fatimaoyiza18@gmail.com>
2022-12-12 02:30:24 -0800

    Tasks

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  *Description: we are unable to use printf so we must use puts to print the string
  *Return: Code should return a 0
  */
-int main(void) 
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 		return (0);
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
  * main: Entry point
+ * Description: printf will be used to print the output with \n as newline
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 		return (0);
