2b5c831d5e79454fd6b6a9e761be9bd4a5317cd2
Fatima <fatimaoyiza18@gmail.com>
2022-12-14 21:47:34 -0800

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,12 +3,11 @@
  * print_alphabet - check the code
  * Return: Always 0
  */
-
 void print_alphabet(void)
 {
 	char i;
 
-		for(i = 'a'; i <= 'z'; i++)
+		for (i = 'a'; i <= 'z'; i++)
 	{
 		_putchar(i);
 	}
