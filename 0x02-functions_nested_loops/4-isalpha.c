39b355f56aeac2de35a2a95e5f17d3c6eb78efdf
Fatima <fatimaoyiza18@gmail.com>
2022-12-15 17:38:57 -0800

    4-isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: character test
+ * Return: 1 if c is a letter and 0 if c is not
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c<='z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
