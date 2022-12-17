0a6a6fc41824412a076c6174a07b3e22b067d091
Fatima <fatimaoyiza18@gmail.com>
2022-12-17 04:24:20 -0800

    8-24

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- * jack_bauver - prints time
+ * jack_bauer - prints time
  * Return: sum of two numbers
  */
 
 void jack_bauer(void)
 {
 	int h, m;
+
 	for (h = 0; h < 24; h++)
 	{
 		for (m = 0; m < 60; m++)
@@ -16,7 +17,7 @@ void jack_bauer(void)
 		_putchar(':');
 		_putchar((m / 10) + '0');
 	 	_putchar((m % 10) + '0');
-       		_putchar('\n');		
+       		_putchar('\n');
 		}
 	}
 }
