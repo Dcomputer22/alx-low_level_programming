b2b236e681ae0796088c25fb7305aefaed448301
Fatima <fatimaoyiza18@gmail.com>
2022-12-18 23:06:15 -0800

    advanced tasks

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - add two integers
+ * Return: the sum of two numbers
+ *@x: The first number to be added
+ *@y: The second number to be added
+ */
+
+int add(int x, int y)
+
+		{
+			return (x + y);
+		}
