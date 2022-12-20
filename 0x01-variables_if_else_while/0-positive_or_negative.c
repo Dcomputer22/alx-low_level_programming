4f1beb9b6b8206f1bb2ede8e42c3d7937e243778
Fatima <fatimaoyiza18@gmail.com>
2022-12-20 10:22:42 -0800

    0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,6 +12,7 @@
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
@@ -21,10 +22,6 @@ int main(void)
 	if (n < 0)
 	{
 		printf("%d is negative\n", n);
-		if (n < 0)
-		{
-			printf("%d is negative\n", n);
 		}
-	}
 	return (0);
 }
