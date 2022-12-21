5e0c68673933932b6cb514deeb3912abdcffb2e2
Fatima <fatimaoyiza18@gmail.com>
2022-12-20 19:00:01 -0800

    Tasks 1-2

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * void swap_int - swaps the values of two integers
+ * Return: 0 success
+ * @a: first int value
+ * @b: second int value
+ */
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+	*a = *b;
+	*b = c;
+}
