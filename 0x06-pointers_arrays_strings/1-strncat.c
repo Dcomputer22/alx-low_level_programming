46e2ecd52c0b647d3e1da17dba1cd503ba818557
Fatima <fatimaoyiza18@gmail.com>
2022-12-22 02:01:13 -0800

    PAS

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * strncat - concatenate two strings
+ * _strncat - concatenate two strings
  * using at most n bytes from src
  * @dest: input value
  * @src: input value
@@ -11,6 +11,7 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i;
 	int j;
+
 	i = 0;
 	while (dest[i] != '\0')
 	{
